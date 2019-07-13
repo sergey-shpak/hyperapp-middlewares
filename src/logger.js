6fccf12d68698a4b077178e033f22f83ed83c600
Sergey <sergey.shpak.web@gmail.com>
2019-07-13 21:41:47 +0300

    Adds verbose param to logger

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,14 @@
 var isArray = Array.isArray
 var isFunction = function(fn){ return typeof fn === 'function' }
 
-export function logger(output){
+export function logger(output, verbose){
   var output = output || console.debug
+  var verbose = verbose || false
   return function(dispatch){
     return function(action, props, obj){
       if (isFunction(action))
         output('Action', action.name, props, obj)
-      else if (isArray(action)) {
+      else if (verbose && isArray(action)) {
         if (isFunction(action[0])) output('Tuple', action[0].name, action[1])
         else if (isArray(action[1])) output('Effect', action[1][0].name, action[1][1])
       } else output('State', action)
